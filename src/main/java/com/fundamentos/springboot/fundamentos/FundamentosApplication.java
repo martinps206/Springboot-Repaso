c4d80d58108e71b1e9d20e66fa3ec8861728a0eb
martinps206 <martinps_cc@hotmail.com>
2022-09-26 01:00:35 -0300

    Primer commit

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
